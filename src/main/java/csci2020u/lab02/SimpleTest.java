6c6c43bff562e3b64af4c064559ac3a4a73283c9
Saawi Baloch <saawibaloch@Saawis-MBP.phub.net.cable.rogers.com>
2022-02-01 11:44:08 -0500

    lab02

---
+++ src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,14 @@
+package csci2020u.lab02;
+
+import java.io.*;
+import java.util.*;
+
+public class SimpleTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+
+}
+
